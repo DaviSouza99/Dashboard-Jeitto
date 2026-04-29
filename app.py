6e195e648550e6c05330b364b1c2013fc46f5c88
DaviSouza99 <davisouza_123@hotmail.com>
2026-04-29 08:01:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,49 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
 
+# ===============================================================
+# CÓDIGO DE VERIFICAÇÃO DE SENHA - COLE ISSO NO TOPO
+# ===============================================================
+
+def check_password():
+
+    """Retorna `True` se o usuário inseriu a senha correta."""
+ 
+    def password_entered():
+
+        """Verifica se a senha inserida pelo usuário está correta."""
+
+        if st.session_state["password"] == st.secrets["PASSWORD"]:
+
+            st.session_state["password_correct"] = True
+
+            del st.session_state["password"]
+
+        else:
+
+            st.session_state["password_correct"] = False
+ 
+    if st.session_state.get("password_correct", False):
+
+        return True
+ 
+    st.text_input(
+
+        "Type your password to continue", type="password", on_change=password_entered, key="password"
+
+    )
+
+    if "password_correct" in st.session_state and not st.session_state["password_correct"]:
+
+        st.error("😕 Incorrect Password")
+
+    return False
+ 
+if not check_password():
+
+    st.stop()
+
+
 # ==========================================
 # CONFIGURAÇÃO DA PÁGINA
 # ==========================================
